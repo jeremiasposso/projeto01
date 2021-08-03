5a59d6788aa7ca62951440c720d82280ae366543
jeremias.posso <jeremias.posso@PLE111D27.alunos.sc.senac.br>
2021-08-02 21:55:24 -0300

    modelos 2

--- Aula Apresentacao/src/util/GeradorUtil.java
+++ Aula Apresentacao/src/util/GeradorUtil.java
@@ -5,6 +5,29 @@ package util;
  * @author jeremias.posso
  */
 public class GeradorUtil {
+    
+    /**
+     * retorna um valor String conforme a qauntidade de número solicitado
+     * 
+     */
+    public String gerarNumero(int qtde){
+        String senha = "";
+        int numero;
+        for(int i = 0; i < qtde; i++){
+            numero = (int) (Math.random() * 10);
+            senha = senha + numero;
+            
+        }
+        return senha;
+    }
+    
+    
+    public String gerarCpf(){
+        return gerarNumero(3) + "." + gerarNumero(3) + "." + gerarNumero(3) +
+                "-" + gerarNumero(2);
+        
+    }
+    
     public double gerarSalario(){
         int numero = (int) (Math.random() * 100000);
         return numero;
@@ -12,8 +35,8 @@ public class GeradorUtil {
     
     
     public static void main(String[] args) {
-       GeradorUtil objetoDessaClasse = new GeradorUtil();
-        System.out.println("Salario: " + objetoDessaClasse.gerarSalario());
-        
+       GeradorUtil util = new GeradorUtil();
+        String cpf = util.gerarCpf();
+        System.out.println("CPF: " + cpf);
     }
 }
