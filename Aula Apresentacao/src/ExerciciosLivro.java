0c8e85c88524a68dddc1689af01b20921e8b602a
jeremiasposso <jereco_sc@hotmail.com>
2021-08-20 03:50:01 -0300

    Treino do livro java.

---
+++ Aula Apresentacao/src/ExerciciosLivro.java
@@ -0,0 +1,23 @@
+public class ExerciciosLivro {
+  public static void main(String[] args) {
+    int x = 1;
+    while( x < 10) {
+      x += +1;
+      if ( x > 3){
+        System.out.println("BIG X");
+        
+      }
+      
+      }
+    }
+  }
+
+
+      
+
+
+    
+ 
+   
+
+ 
